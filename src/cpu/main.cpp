c4aa9e58edd08dc9bb90754d129c2acd2f9de2da
Joseph Wu <josephwu.wyz@hotmail.com>
2016-02-01 16:05:26 -0800

    fix conflit

--- src/cpu/main.cpp
+++ src/cpu/main.cpp
@@ -7,23 +7,12 @@ int main() {
 
 	cout << "A) CPU, Scheduling, and OS Services starts:" << endl;
 	// benchmark.warmup();
-<<<<<<< HEAD
-	// benchmark.measurementOverhead(file);
-	 benchmark.procedureCallOverhead(file);
-	// benchmark.systemCallOverhead(file);
-	//benchmark.taskCreationTime(file);
-    benchmark.contextSwitchOverhead(file);
-=======
 	benchmark.measurementOverhead(file);
 	benchmark.procedureCallOverhead(file);
 	benchmark.systemCallOverhead(file);
 	benchmark.taskCreationTime(file);
+  benchmark.contextSwitchOverhead(file);
 
->>>>>>> cd86c1b8a437d2634cfd8669383f2fb039a8dc59
 	cout << "A) Completes" << endl;
-
-
-
-
 	return 0;
 }
