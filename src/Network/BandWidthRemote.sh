d9e7917426636acdb2777686d1c71acd19fb28b5
joseph <josephwu.wyz@hotmail.com>
2016-03-13 22:43:08 -0700

    remove redundant

---
+++ src/Network/BandWidthRemote.sh
@@ -0,0 +1,9 @@
+./bandWidthClient 192.168.1.2 1234 512
+./bandWidthClient 192.168.1.2 1234 2048
+./bandWidthClient 192.168.1.2 1234 8192
+./bandWidthClient 192.168.1.2 1234 32768
+./bandWidthClient 192.168.1.2 1234 131072
+./bandWidthClient 192.168.1.2 1234 524288  #512kb
+./bandWidthClient 192.168.1.2 1234 2097152 #2MB
+./bandWidthClient 192.168.1.2 1234 8388608 #8MB
+./bandWidthClient 192.168.1.2 1234 33554432 #32MB
